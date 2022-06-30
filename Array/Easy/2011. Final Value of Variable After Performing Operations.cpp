9aa762444ae6d8033cbf0294ae5d0229e38148b2
Sarvesh Mishra <officialsmishra@gmail.com>
2022-06-30 22:16:11 +0530

    add c++ starting

---
+++ Array/Easy/2011. Final Value of Variable After Performing Operations.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int finalValueAfterOperations(int x) {
+        cout << "x " << x << endl;
+        return x;
+    }
+};
+
+int main () {
+    Solution s;
+    s.finalValueAfterOperations(5);
+    return 0;
+}
