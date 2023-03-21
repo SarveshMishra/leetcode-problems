bcf44cd695a8c16267e998bb5c2210b7605413a6
Sarvesh Mishra <officialsmishra@gmail.com>
2023-03-21 18:34:12 +0530

    add problem 1757
    Signed-off-by: Sarvesh Mishra <officialsmishra@gmail.com>

---
+++ MySQL/1757. Recyclable and Low Fat Products.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+# https://leetcode.com/problems/recyclable-and-low-fat-products/?envType=study-plan&id=sql-i
+
+select product_id from Products where low_fats = 'Y' and recyclable = 'Y';
